4313e6ab67d0a582460485dedd0a2fefd3b288f0
Banff International Research Station <brentk@birs.ca>
2022-01-07 17:12:59 -0700

    remove globe.png from manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,6 @@
 //= link_directory ../javascripts
 //= link_directory ../stylesheets
 //= link_tree ../images
-//= link ../images/globe.png
 //= link application.css
 //= link administrate/application.css
 //= link administrate/application.js
